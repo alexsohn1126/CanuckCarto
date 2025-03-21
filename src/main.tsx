dad2f042f3fd7296ad24058e91331cfc25ae7912
Alex Sohn <alexsohn1126@gmail.com>
2025-03-21 12:58:36 -0400

    fix css import

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "leaflet/dist/leaflet.css";
-import "react-leaflet-markercluster/styles";
+import "react-leaflet-markercluster/dist/styles.min.css";
 import App from "./App.tsx";
 import ModalProvider from "./modals/ModalContext.tsx";
 
